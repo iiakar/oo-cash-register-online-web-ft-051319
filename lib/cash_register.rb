2943716f9fc672d60cc4e88f5c6b4a22f948270b
Ibrahim  Akar <ibsakar@gmail.com>
2019-06-02 19:54:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
+class CashRegister
+  attr_accessor :discount, :total 
 
+
+def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    #@cart = []
+  end
+  
+  def add_item(title,price,quantity = 1) 
+    @total += price * quantity
+  end 
+
+  
+
+
+
+
+end
+  
